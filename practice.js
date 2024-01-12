d89834659fa426e06df2f22ad952d8d597afdee6
ageeha927 <141285328+ageeha927@users.noreply.github.com>
2024-01-11 18:25:12 -0700

    .

---
+++ practice.js
@@ -0,0 +1,9 @@
+// console.log(typeof('17'))
+// console.log(typeof(+0))
+// console.log(typeof(-0))
+// console.log(typeof(17))
+// console.log(String(null))
+// console.log(100%7)
+
+// Math Solver
+let a = prompt('What number would you like to input?')
---
+++ quiz.js
@@ -0,0 +1,15 @@
+// alert('Welcome to My Slime Rancher Quiz!')
+// let question1 = prompt("What is the primary resource used for building infrastructure in Slime Rancher?")
+// let question2 = prompt("Which biome in Slime Rancher is known for its phosphorescent landscape and crystal slimes?")
+// let question3 = prompt("What is the main method for capturing and corralling slimes on the ranch?")
+// let question4 = prompt("Which slime species is known for producing valuable plorts that change in value over time?")
+// let question5 = prompt("What is the primary currency used in Slime Rancher to purchase upgrades and expansions?")
+// let question6 = prompt("Which type of slime has the ability to transform into other slimes when exposed to different foods?")
+// let question7 = prompt("What is the name of the friendly robot character that assists players with ranching advice?")
+// let question8 = prompt("In Slime Rancher, what is the purpose of the Slime Science Lab?")
+// let question9 = prompt("Which vegetable is a rock slimes favorite?")
+// let question10 = prompt("What is the primary threat to a rancher's slimes, created when different slime types combine?")
+
+
+let score = document.getElementById("score");
+score.textContent = 'Hello'
